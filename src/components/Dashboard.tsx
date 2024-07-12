0283fc1129cd37d216d1de42f85a98e80c143b61
Divyam19 <divyamgupta19@gmail.com>
2024-07-13 01:35:12 +0530

    auth, shadcn

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import React from 'react'
+import { Button } from './ui/button';
+
+const Dashboard = () => {
+  const { data: session } = useSession();
+  return (
+    <div>
+      {session?.user ? (
+        <>
+            <h1>Session exists</h1>
+            <p>{JSON.stringify(session)}</p>
+            <Button variant={'ghost'} onClick={()=>signOut()} className='rounded-xl bg-orange-600 hover:bg-orange-400'>Signout</Button>
+        </>
+      ) : (
+        <>
+            <h1>Session not exists</h1>
+            <Button variant={'ghost'} className='bg-orange-300 rounded-xl hover:bg-orange-500' onClick={()=>signIn('google')}>Sign in with Google</Button>
+            <Button variant={'ghost'} className='bg-orange-300 rounded-xl hover:bg-orange-500' onClick={()=>signIn('github')}>Sign in with Github</Button>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default Dashboard
